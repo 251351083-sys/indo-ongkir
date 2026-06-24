93f952a354d24f1690e372d3bb15955949a49ace
251351083-sys <251351083@wastukancana.ac.id>
2026-06-24 11:28:51 +0700

    fix: memperbaiki typo pembuka tag PHP di web.php dan sinkronisasi rute

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,17 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+// Pastikan di bagian atas file route sudah ada import ini:
 use App\Http\Controllers\CartController;
 
-// Rute Tampilan Utama (Membaca API RajaOngkir & Keranjang)
-Route::get('/', [CartController::class, 'index'])->name('shipping.index');
-Route::get('/get-cities/{province_id}', [CartController::class, 'getCities']);
+Route::middleware('auth')->group(function () {
+    
+    // 1. UBAH SEMUA SHIPPINGCONTROLLER MENJADI CARTCONTROLLER
+    Route::get('/shipping', [CartController::class, 'index'])->name('shipping.index');
+    Route::get('/get-cities/{province_id}', [CartController::class, 'getCities']);
+    Route::post('/check-cost', [CartController::class, 'checkCost']);
 
-// Modul Fitur Keranjang Belanja Berdasarkan Revisi
-Route::post('/cart/add', [CartController::class, 'addToCart'])->name('cart.add');
-Route::get('/cart/clear', [CartController::class, 'clearCart'])->name('cart.clear');
-Route::get('/cart/delete/{id}', [CartController::class, 'removeFromCart']);
-Route::get('/cart/increase/{id}', [CartController::class, 'increaseQty']);
-Route::get('/cart/decrease/{id}', [CartController::class, 'decreaseQty']);
-Route::get('/switch-role/{role}', [CartController::class, 'switchRole']);
-// Modul CRUD Manajemen Produk
-Route::post('/product/store', [CartController::class, 'storeProduct'])->name('product.store');
-Route::get('/product/delete/{id}', [CartController::class, 'deleteProduct']);
-
-// Modul Hitung Cost & Checkout
-Route::post('/checkout', [CartController::class, 'checkout'])->name('checkout');
+    // Rute bawaan lainnya...
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
